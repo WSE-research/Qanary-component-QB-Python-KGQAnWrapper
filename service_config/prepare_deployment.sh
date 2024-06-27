832a2623e8c749e328ab4e09c8710b2b43eb7736
heinpa <heipa@posteo.de>
2024-06-27 09:28:55 +0200

    set placeholders

--- service_config/prepare_deployment.sh
+++ service_config/prepare_deployment.sh
@@ -5,14 +5,14 @@ if [ -z "$SPRING_BOOT_ADMIN_USERNAME" ]; then
   echo "SPRING_BOOT_ADMIN_USERNAME is not set. Check your secrets."
   exit 2
 else
-  sed -i "s/SPRING_BOOT_ADMIN_USERNAME/$SPRING_BOOT_ADMIN_USERNAME/g" ./service_config/files/qb-kgqanwrapper-dbpedia
-  sed -i "s/SPRING_BOOT_ADMIN_USERNAME/$SPRING_BOOT_ADMIN_USERNAME/g" ./service_config/files/qb-kgqanwrapper-wikidata
+  sed -i "s/SPRING_BOOT_ADMIN_USERNAME_PLACEHOLDER/$SPRING_BOOT_ADMIN_USERNAME/g" ./service_config/files/qb-kgqanwrapper-dbpedia
+  sed -i "s/SPRING_BOOT_ADMIN_USERNAME_PLACEHOLDER/$SPRING_BOOT_ADMIN_USERNAME/g" ./service_config/files/qb-kgqanwrapper-wikidata
 fi
 
 if [ -z "$SPRING_BOOT_ADMIN_PASSWORD" ]; then
   echo "SPRING_BOOT_ADMIN_PASSWORD is not set. Check your secrets."
   exit 2
 else
-  sed -i "s/SPRING_BOOT_ADMIN_PASSWORD/$SPRING_BOOT_ADMIN_PASSWORD/g" ./service_config/files/qb-kgqanwrapper-dbpedia
-  sed -i "s/SPRING_BOOT_ADMIN_PASSWORD/$SPRING_BOOT_ADMIN_PASSWORD/g" ./service_config/files/qb-kgqanwrapper-wikidata
+  sed -i "s/SPRING_BOOT_ADMIN_PASSWORD_PLACEHOLDER/$SPRING_BOOT_ADMIN_PASSWORD/g" ./service_config/files/qb-kgqanwrapper-dbpedia
+  sed -i "s/SPRING_BOOT_ADMIN_PASSWORD_PLACEHOLDER/$SPRING_BOOT_ADMIN_PASSWORD/g" ./service_config/files/qb-kgqanwrapper-wikidata
 fi
