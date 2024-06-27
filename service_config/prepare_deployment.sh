9bfbdd7d2b6159f01133b3aef78a35116ab3f130
heinpa <heipa@posteo.de>
2024-06-27 09:15:38 +0200

    correct username variable

--- service_config/prepare_deployment.sh
+++ service_config/prepare_deployment.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # replace secrets
-if [ -z "$SPRING_BOOT_ADMIN_USER" ]; then
-  echo "SPRING_BOOT_ADMIN_USER is not set. Check your secrets."
+if [ -z "$SPRING_BOOT_ADMIN_USERNAME" ]; then
+  echo "SPRING_BOOT_ADMIN_USERNAME is not set. Check your secrets."
   exit 2
 else
-  sed -i "s/SPRING_BOOT_ADMIN_USER/$SPRING_BOOT_ADMIN_USER/g" ./service_config/files/qb-kgqanwrapper-dbpedia
-  sed -i "s/SPRING_BOOT_ADMIN_USER/$SPRING_BOOT_ADMIN_USER/g" ./service_config/files/qb-kgqanwrapper-wikidata
+  sed -i "s/SPRING_BOOT_ADMIN_USERNAME/$SPRING_BOOT_ADMIN_USERNAME/g" ./service_config/files/qb-kgqanwrapper-dbpedia
+  sed -i "s/SPRING_BOOT_ADMIN_USERNAME/$SPRING_BOOT_ADMIN_USERNAME/g" ./service_config/files/qb-kgqanwrapper-wikidata
 fi
 
 if [ -z "$SPRING_BOOT_ADMIN_PASSWORD" ]; then
